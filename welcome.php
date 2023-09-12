5c974b4a98db9c7675faf5b5ce77ea3d184ea98d
kibeert <ckibertkoech@gmail.com>
2023-09-12 14:25:24 +0300

    The diff adds the following lines of code:
    ```
    echo "<h2>this is fun</h2>"."<br>";
        echo "Hello World"."<br>";
        print "Hello World"."<br>";
        print "<h2>Studying php is fun and cool</h2>";
        $x1 = 1234;
        echo "$x1"."<br>";
        var_dump($x1)."<br>";
        $y2= 2.3;
        print "$y2"."<br>";
        var_dump($y2)."<br>";
        $cars = array("Volvo", "Mercedes", "BMW", "Toyota")."<br>";
        var_dump($cars);
        echo ($cars)
    ``` (generated by useblackbox.ai )

--- welcome.php
+++ welcome.php
@@ -18,6 +18,19 @@
     $x= 10;
     $y= 6.5;
     echo "the value of x and y is"." ".$x+$y;
+    echo "<h2>this is fun</h2>"."<br>";
+    echo "Hello World"."<br>";
+    print "Hello World"."<br>";
+    print "<h2>Studying php is fun and cool</h2>";
+    $x1 = 1234;
+    echo "$x1"."<br>";
+    var_dump($x1)."<br>";
+    $y2= 2.3;
+    print "$y2"."<br>";
+    var_dump($y2)."<br>";
+    $cars = array("Volvo", "Mercedes", "BMW", "Toyota")."<br>";
+    var_dump($cars);
+    echo ($cars)
     ?>
 </body>
 </html>
