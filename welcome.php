f33f83608d96a5be9fae1b9a24027bed85e75978
kibeert <ckibertkoech@gmail.com>
2023-09-12 13:54:07 +0300

    first commit

---
+++ welcome.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first Php code</h1>
+    <?php
+    echo "Hello World";
+    echo "Date is:" ." ".date("j-m-y,h:i:s"). "<br>";
+    $color = "Red";"<br>";
+    echo "$color" ."<br>";
+    $today = "Monday" ."<br>";
+    echo "Today is on a". " ".$today;
+
+    
+    ?>
+</body>
+</html>
